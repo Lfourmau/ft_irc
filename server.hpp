3adafb93628677aa2e7c43a90d051fd1b163f018
Nicolas Catriens <nicolas.catriens@gmail.com>
2022-06-27 11:30:17 +0200

    deleted an unused function because otherwise lolo doesnt accept my PR

--- server.hpp
+++ server.hpp
@@ -54,7 +54,6 @@ class server
 		std::string 		get_ip();
 		void				print_channels();
 		void				print_users();
-		void				delete_user(int userFd);
 		std::vector<user*> 	&get_users();
 		std::vector<channel> 	&get_channels();
 		std::vector<std::string>	parsing_join_input(std::string& strings);
