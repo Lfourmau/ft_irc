1d13a28f1787981045d581481c0c2070f9d1e88d
Chafik Taleb <taleb.cas@gmail.com>
2022-06-29 13:52:57 +0200

    started bot skeleton

---
+++ bot/src/main.cpp
@@ -0,0 +1,24 @@
+#include "bot.hpp"
+
+int main(int argc, char** argv)
+{
+	std::string	server;
+	std::string	port;
+	std::string	nick;
+	std::string	channel;
+	std::string	buffer;
+	int			msg_type;
+
+	std::stringstream ss;
+	ss << ;
+	users = ss.str();
+
+	if (argc != 4) {
+		std::cerr << "Usage: ./ircbot <server> <port> <password>" << std::endl;
+		exit(-1);
+	}
+	server = argv[1];
+	port = argv[2];
+	
+
+}
