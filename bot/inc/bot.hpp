013b894f9fcd0b5998fabc74e00158d4d1e00013
Chafik Taleb <taleb.cas@gmail.com>
2022-06-29 14:51:44 +0200

    finished basic skeleton for the bot

--- bot/inc/bot.hpp
+++ bot/inc/bot.hpp
@@ -1,3 +1,23 @@
 #include <string>
 #include <iostream>
-#include <sstream>
+#include <vector>
+// #include <sstream>
+
+#define IRC_RECIEVE_ERROR 1//tochange
+#define	PREFIX !
+
+class Bot
+{
+public:
+	Bot();
+	~Bot();
+
+	bool		connect(std::string server, std::string port, std::string password);
+	bool		identify(std::string nick);
+	bool		join(std::string channel);
+	std::string	get_data();
+	int			get_last_error();
+	bool		parser(std::string buffer, std::vector<std::string> *cmd_list);
+	void		notice(std::string message, std::string channel);
+	void		quit();
+};
--- bot/src/main.cpp
+++ bot/src/main.cpp
@@ -2,16 +2,14 @@
 
 int main(int argc, char** argv)
 {
-	std::string	server;
-	std::string	port;
-	std::string	nick;
-	std::string	channel;
-	std::string	buffer;
-	int			msg_type;
-
-	std::stringstream ss;
-	ss << ;
-	users = ss.str();
+	std::string					server;
+	std::string					port;
+	std::string 				password;
+	std::string					nick;
+	std::string					channel;
+	std::string					buffer;
+	std::vector<std::string>	cmd_list;
+	bool						end_bot = false;
 
 	if (argc != 4) {
 		std::cerr << "Usage: ./ircbot <server> <port> <password>" << std::endl;
@@ -19,6 +17,42 @@ int main(int argc, char** argv)
 	}
 	server = argv[1];
 	port = argv[2];
-	
+	password = argv[3];
+	channel = "#botroom";
+	nick = "ircbot";
 
+	Bot	ircbot;
+	if (!ircbot.connect(server, port, password))
+	{
+		std::cerr << "Could not connect to server." << std::endl;
+		exit(-1);
+	}
+	if (!ircbot.identify(nick))
+	{
+		std::cerr << "Could not register to the server." << std::endl;
+		exit(-1);
+	}
+	if (!ircbot.join(channel))
+	{
+		std::cerr << "Could not join the channel." << std::endl;
+		exit(-1);
+	}
+	while (end_bot == false)
+	{
+		buffer = ircbot.get_data();
+		if (ircbot.get_last_error() != IRC_RECIEVE_ERROR)
+		{
+			if (ircbot.parser(buffer, &cmd_list))
+			{
+				if (cmd_list[0] == "QUIT")
+				{
+					ircbot.notice("IRCBot Quitting...", channel);
+					end_bot == true;
+				}
+			}
+			cmd_list.clear();
+		}
+	}
+	ircbot.quit();
+	return 0;
 }
